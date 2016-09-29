9f8f0394f655f38791e323be053dd39f0475a8de
Dennis Parussini <thedan84@bitbucket.org>
2016-09-29 18:35:55 +0200

    Initial Commit

---
+++ Movie Favorites/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Movie Favorites
+//
+//  Created by Dennis Parussini on 29-09-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Movie Favorites/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Movie Favorites
+//
+//  Created by Dennis Parussini on 29-09-16.
+//  Copyright © 2016 Dennis Parussini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
